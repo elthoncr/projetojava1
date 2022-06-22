e1ff6c117df75da025e0e5a50878f106d1927969
Elthon Rodrigues <elthon.c.r@gmail.com>
2022-06-22 10:17:20 -0300

    Criação do projeto.

---
+++ primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -0,0 +1,12 @@
+package cursojava.executavel;
+
+public class PrimeiraClasseJava {
+
+	/*Main é um metodo auto executavel em Java*/
+	public static void main(String[] args) {
+		
+		System.out.println("Meu primeiro programa em Java");
+		
+	}
+	
+}
